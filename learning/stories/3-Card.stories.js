03736cd5c71be1bfd1491d1a12e27de30a76014d
Strat Parrott <strat@thejuncture.com>
2020-03-18 09:20:44 -0400

    storybook react

---
+++ learning/stories/3-Card.stories.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import '../css/moflo.css'; // Tell webpack that x uses these styles
+
+export default { title: 'Card', };
+
+export const Card = () =>
+    <section className="card">
+        <div className="information">
+            <div className="balance">
+                <div className="title">Float</div>
+                <div className="amount">$0,000</div>
+                <div className="date">Until 2020-04-13 (60 Days)</div>
+            </div>
+
+            <div className="balance">
+                <div className="title">Current Balance</div>
+                <div className="amount">$0,000</div>
+                <div className="date">As of 2020-04-13 (60 Days)</div>
+            </div>
+
+        </div>
+    </section>
+
+export const CardSuccess = () =>
+    <section className="card">
+        <div className="information">
+            <div className="balance">
+                <div className="title success">Float</div>
+                <div className="amount success">$0,000</div>
+                <div className="date">Until 2020-04-13 (60 Days)</div>
+            </div>
+
+            <div className="balance">
+                <div className="title">Current Balance</div>
+                <div className="amount">$0,000</div>
+                <div className="date">As of 2020-04-13 (60 Days)</div>
+            </div>
+
+        </div>
+    </section>
+
+export const CardError = () =>
+    <section className="card">
+        <div className="information">
+            <div className="balance">
+                <div className="title">Float</div>
+                <div className="amount">$0,000</div>
+                <div className="date">Until 2020-04-13 (60 Days)</div>
+            </div>
+
+            <div className="balance">
+                <div className="title error">Current Balance</div>
+                <div className="amount error">-$0,000</div>
+                <div className="date error">As of 2020-04-13 (60 Days)</div>
+            </div>
+
+        </div>
+    </section>
+    ;
+
